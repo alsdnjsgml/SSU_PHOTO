62ec6fe30260edd4d4eed145e0a134abf6579f43
심세진 <ssj259@gmail.com>
2021-05-04 15:41:13 +0000

    ~5/4

---
+++ app_colab.py
@@ -0,0 +1,69 @@
+from flask import Flask , render_template ,request
+import os, sys
+import matplotlib.pyplot as plt
+import style_transfer
+from flask import Flask, url_for, redirect, render_template, request
+from flask_ngrok import run_with_ngrok
+import os
+import time
+import numpy as np
+
+TEMPLATE_FOLDER = '/content/SSU_PHOTO/templates' # 임의의 경로
+STATIC_FOLDER = '/content/SSU_PHOTO/static' # 임의의 경로
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder = STATIC_FOLDER)
+run_with_ngrok(app_colab)   #starts ngrok when the app is run
+
+UPLOAD_FOLDER = '/content/SSU_PHOTO/static/image/upload'
+style =""
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route('/about')
+def about():
+  return render_template('about_page.html')
+
+@app.route('/scrath')
+def scrath():
+  return render_template('scrath_page.html')
+
+@app.route('/artist')
+def artist():
+  return render_template('artist_page.html')
+
+@app.route('/gallery')
+def gallery():
+  return render_template('gallery_page.html')
+    
+    
+@app.route("/success", methods=['POST'])
+def upload_file():
+      a = str(np.random.randint(1,100))
+      content = request.files['file1']
+      style = request.form.get('style')
+      content.save(os.path.join(UPLOAD_FOLDER+ '/'+a+'content.jpg'))
+
+      #load in content and style image
+      content = style_transfer.load_image(UPLOAD_FOLDER+'/'+a+'content.jpg')
+      
+      vgg = style_transfer.model()
+
+      # #Resize style to match content, makes code easier
+      style = style_transfer.load_image(UPLOAD_FOLDER+'/s'+ str(style)+'.jpg', shape=content.shape[-2:])
+      
+      target = style_transfer.stylize(content,style,vgg)
+      x = style_transfer.im_convert(target)
+      plt.imsave(UPLOAD_FOLDER+'/'+a+'target.png',x)
+
+      return render_template('artist_page_scc.html', content_file= 'image/upload/'+a+'content.jpg', target= 'image/upload/'+a+'target.png')
+
+							
+if __name__ == '__main__':
+    app_colab.run()
+    
+    
+    # 참고 1) 파일 저장 위치 (FLASK 문법)
+    #      ㄴ templates -html / static : image, css, js
+    #     2) 홈페이지는 상단에 동그라미 아이콘 클릭시 시작 : 여러명이 실행 시키면 구동 발생 참고
+    #     3) input size = 400 x 400 이하, 풍경 사진 추천(차이가 확명하게 보임)   test 
