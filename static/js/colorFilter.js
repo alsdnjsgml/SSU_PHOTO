a5f388abb4dbecdb9e276a4baaad15cb0438138a
19)민원희 <52127527+alsdnjsgml@users.noreply.github.com>
2021-05-26 22:41:31 +0900

    Update colorFilter.js
    Add saveImage

--- static/js/colorFilter.js
+++ static/js/colorFilter.js
@@ -1,23 +1,23 @@
 console.log('filter init hi');
+// 출처: https://dev-momo.tistory.com/entry/Javascript-Image-Filter-만들기 [Programming Note]
 
 // canvas 객체 생성
-var canvas = $('#picture_canvas')[0];
-var ctx = canvas.getContext('2d');
+var canvas_color = $('#canvas_color')[0];
+var colorCTX = canvas_color.getContext('2d');
+var canvas_gray = $('#canvas_gray')[0];
+var grayCTX = canvas_gray.getContext('2d');
+
+var isUpload = false;
 
 function drawImageData(image) {
-    image.height *= canvas.offsetWidth / image.width;
-    image.width = canvas.offsetWidth;
+    image.height = (canvas_color.width / image.width) * image.height ;
+    image.width = canvas_color.width;
 
-    if(image.height > canvas.offsetHeight){
-        image.width *= canvas.offsetHeight / image.height;
-        image.height = canvas.offsetHeight;
-    }
-
-    ctx.drawImage(image, 0, 0, image.width, image.height);
+    colorCTX.drawImage(image, 0, 0, image.width, image.height);
+    grayCTX.drawImage(image, 0, 0, image.width, image.height);
 }
 
 // click input button
-var originalPixels;
 $('#fileupload').on('change', function (e) {
     var file = e.target.files[0];
     var fileReader = new FileReader();
@@ -27,45 +27,84 @@ $('#fileupload').on('change', function (e) {
         image.src = e.target.result;
         image.onload = function () {
             drawImageData(image);
-            originalPixels = ctx.getImageData(0,0, canvas.width, canvas.height);
         }
+        isUpload = true;
     };
     fileReader.readAsDataURL(file);
 });
 
 
-$('#selectColor').on('click', function () {
-    var filteredData;
+$('input[name=style]').on('click', function () {
+
+    if (!isUpload) {
+        alert('이미지를 업로드 해주세요!');
+        return
+    }
+    
     // imageData를 가져온다.
-    var pixels = originalPixels;
+    var colorPixels = colorCTX.getImageData(0,0, canvas_color.width, canvas_color.height);
+    var grayPixels = grayCTX.getImageData(0,0, canvas_color.width, canvas_color.height);
+
     // image processing
-    var selectColor = $('input[name=color]:checked').val()
-    if (selectColor === 'red') {
-        filteredData = redFilter(pixels);
+    var selectColor = $('input[name=style]').val()
+    if (selectColor === 'org') {
+    }
+    else if (selectColor === 'red') {
+        redFilter(colorPixels);
     }
     else if (selectColor === 'blue') {
-        filteredData = blueFilter(pixels);
+        blueFilter(colorPixels);
     }
     else if (selectColor === 'green') {
-        filteredData = greenFilter(pixels);
+        greenFilter(colorPixels);
     }
+
+    grayscaleFilter(grayPixels);
+
 
     // Canvas에 다시 그린다.
-    ctx.putImageData(filteredData, 0 , 0);
+    colorCTX.putImageData(colorPixels, 0 , 0);
+    grayCTX.putImageData(grayPixels, 0 , 0);
+
+    // Canvas 이미지 서버 저장
+    saveImage(canvas_color);
 });
 
-// Filters
-function invertFilter(pixels) {
-    var d = pixels.data;
-    for(var i=0; i<pixels.data.length; i+=4 ){
-        d[i] = 255 - d[i];     // R
-        d[i+1] = 255 - d[i+1]; // G
-        d[i+2] = 255 - d[i+2]; // B
-        d[i+3] = 255;          // Alpha
+// 캔버스의 이미지 저장
+// 출처: https://webisfree.com/2020-08-05/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-canvas-%EC%9A%94%EC%86%8C%EB%A5%BC-%EC%9D%B4%EB%AF%B8%EC%A7%80%EB%A1%9C-%EC%A0%80%EC%9E%A5-%ED%9B%84-%EC%84%9C%EB%B2%84%EC%97%90-ajax%EB%A1%9C-%EC%A0%80%EC%9E%A5%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
+// 출처: https://samanoske.tistory.com/94
+function saveImage(canvas) {
+    const imgBase64 = canvas.toDataURL('image/jpeg', 'image/octet-stream');
+    const decodImg = atob(imgBase64.split(',')[1]);
+
+    let array = [];
+    for (let i = 0; i < decodImg .length; i++) {
+    array.push(decodImg .charCodeAt(i));
     }
-    return pixels;
+    const file = new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+    const fileName = 'canvas_img_' + '.jpg';
+    
+    let formData = new FormData();
+    formData.append('file', file, fileName);
+
+    $.ajax({
+        type: 'POST',
+        url: '/scrath_out',
+        data: formData,
+        enctype: 'multipart/form-data', 
+        processData: false,
+        contentType: false,
+        beforeSend: function() { //로딩이미지 보여주기
+            $('#loading').show();
+        },
+        success : function(response){
+            document.write(response);
+            $('#loading').hide();
+            }
+      })
 }
 
+// Filters
 function grayscaleFilter(pixels) {
     var d = pixels.data;
     for(var i =0; i< d.length; i+=4){
@@ -86,14 +125,20 @@ function redFilter(pixels) {
         var g = d[i+1];
         var b = d[i+2];
 
-        // d[i] = r*0.9588 + g*0.7044 + b*0.1368;
-        // d[i+1] = r*0.2990 + g*0.5870 + b*0.1140;
-        // d[i+2] = r*0.2392 + g*0.4696 + b*0.0912;
-
+        // if (r>240&&g>240&&b>240) {
+        //     continue;
+        // }
 
         d[i] = r*0.9 + g*0.1 + b*0.1;
         d[i+1] = r*0.2 + g*0.2 + b*0.2;
         d[i+2] = r*0.2 + g*0.2 + b*0.2;
+
+        if (b>r) {
+            d[i] = b;
+        }
+        if (g>r) {
+            d[i] = g;
+        }
     }
     return pixels;
 }
@@ -108,6 +153,14 @@ function blueFilter(pixels) {
         d[i] = r*0.2 + g*0.2 + b*0.2;
         d[i+1] = r*0.2 + g*0.2 + b*0.2;
         d[i+2] = r*0.1 + g*0.1 + b*0.9;
+
+
+        if (r>b) {
+            d[i+2] = r;
+        }
+        if (g>b) {
+            d[i+2] = g;
+        }
     }
     return pixels;
 }
@@ -122,6 +175,14 @@ function greenFilter(pixels) {
         d[i] = r*0.2 + g*0.2 + b*0.2;
         d[i+1] = r*0.1 + g*0.9 + b*0.1;
         d[i+2] = r*0.2 + g*0.2 + b*0.2;
+
+
+        if (r>g) {
+            d[i+1] = r;
+        }
+        if (b>g) {
+            d[i+1] = b;
+        }
     }
     return pixels;
 }
